1541d26692c03867c6716c43fc7804a327da64e1
moliver226 <marcusoliver226@gmail.com>
2022-07-28 21:28:12 -0400

    Confirmation page created.

---
+++ src/components/TestDrive/Confirmation.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export const Confirmation = ({ info }) => {
+  // const info = { firstName, lastName, email, phone, formattedDate, selectedTime};
+
+  return (
+    <>
+      {info ? (
+        <div className="test-drive-confirmation-page">
+          <h5>Appointment Details</h5>
+          <div className="test-drive-confirmation-page-labels">
+             <p><strong>Name: </strong></p>
+             <p>{info.firstName + " " + info.lastName}</p>
+          </div>
+          <div className="test-drive-confirmation-page-labels">
+             <p><strong>Email: </strong></p>
+             <p>{info.email}</p>
+          </div>
+          <div className="test-drive-confirmation-page-labels">
+             <p><strong>Phone: </strong></p>
+             <p>{info.phone}</p>
+          </div>
+          <div className="test-drive-confirmation-page-labels">
+             <p><strong>Date: </strong></p>
+             <p>{info.formattedDate + " " + info.selectedTime}</p>
+          </div>
+        </div>
+      ) : null}
+    </>
+  );
+};
